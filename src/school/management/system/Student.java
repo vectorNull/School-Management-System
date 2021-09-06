3ef09afbbcb9d27b23195685ecd037a6cd2a9e3a
vectorNull <jose.rodriguez@prominentcode.com>
2021-09-05 21:40:31 -0700

    Initial commit

---
+++ src/school/management/system/Student.java
@@ -0,0 +1,16 @@
+package school.management.system;
+
+/**
+ * Created by Jose Rodriguez on 9/5/2021
+ * This class is responsible for keeping track of student name, id, grades, fees, and fees paid.
+ *
+ */
+
+public class Student {
+
+    private int id;
+    private String name;
+    private int grade;
+
+
+}
