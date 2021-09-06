79d48a61d244a30644ccbd193caf59d550556f7b
vectorNull <jose.rodriguez@prominentcode.com>
2021-09-05 22:01:43 -0700

    Update Student class

--- src/school/management/system/Student.java
+++ src/school/management/system/Student.java
@@ -11,6 +11,68 @@ public class Student {
     private int id;
     private String name;
     private int grade;
+    private double feesPaid;
+    private double feesTotal;
+
+    /**
+     *
+     * @param id id for student
+     * @param name name of student
+     * @param grade grade of student
+     */
+    public Student (int id, String name, int grade) {
+
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+
+    }
+    // Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public double getFeesPaid() {
+        return feesPaid;
+    }
+
+    public double getFeesTotal() {
+        return feesTotal;
+    }
+
+    // Getters
 
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public void setFeesPaid(double feesPaid) {
+        this.feesPaid = feesPaid;
+    }
+
+    public void setFeesTotal(double feesTotal) {
+        this.feesTotal = feesTotal;
+    }
+
+    public void updateFeesPaid(int fees) {
+        feesPaid += fees;
+    }
 }
